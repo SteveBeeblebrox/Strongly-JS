b3b5ea38b21bc9c810df4f86be1d0f3d5491a617
Tim Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2020-11-19 15:19:44 -0700

    Added some default types

--- strongly.js
+++ strongly.js
@@ -21,9 +21,29 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+class StronglyJSType {
+  #a; #b; #c;
+  constructor(fallback, name = typeof fallback, test = (o) => typeof o === typeof fallback) {
+    if(typeof name !== 'string') throw new TypeError(`Cannot convert '${typeof b}' to 'string'`);
+    if(typeof test !== 'function') throw new TypeError(`Cannot convert '${typeof c}' to 'string'`);
+    this.#a = fallback;
+    this.#b = name;
+    this.#c = test;
+  }
+  get fallback() { return this.#a}
+  get name() { return this.#b}
+  allows(o) {return this.#c(o)}
+}
 class StronglyJS {
   static #TYPES = {
-    __proto__: null
+    __proto__: null,
+	NUMBER: new StronglyJSType(0),
+	BOOLEAN: new StronglyJSType(false),
+	STRING: new StronglyJSType(''),
+	FUNCTION: new StronglyJSType(function() {}),
+	BIGINT: new StronglyJSType(0n),
+	UNDEFINED: new StronglyJSType(undefined),
+	SYMBOL: new StronglyJSType(undefined, 'symbol', (o) => typeof o === 'symbol')
   }
   static get TYPES() {return {...this.#TYPES}}
   static strongProp(on, name, strongValue) {
@@ -38,17 +58,4 @@ class StronglyJS {
     });
     return on[name];
   }
-}
-class StronglyJSType {
-  #a; #b; #c;
-  constructor(fallback, name = typeof fallback, test = (o) => typeof o === typeof fallback) {
-    if(typeof name !== 'string') throw new TypeError(`Cannot convert '${typeof b}' to 'string'`);
-    if(typeof test !== 'function') throw new TypeError(`Cannot convert '${typeof c}' to 'string'`);
-    this.#a = fallback;
-    this.#b = name;
-    this.#c = test;
-  }
-  get fallback() { return this.#a}
-  get name() { return this.#b}
-  allows(o) {return this.#c(o)}
 }
