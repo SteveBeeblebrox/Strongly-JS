d611032e570e9856fd4d9a35b43c4532ce07ac93
Tim Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2020-11-28 11:33:24 -0700

    Internal name changes

--- strongly.js
+++ strongly.js
@@ -22,19 +22,19 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 class StronglyJSType {
-  #a; #b; #c; #d;
+  #f; #n; #t; #w;
   constructor(fallback, name = typeof fallback, test = (o) => typeof o === typeof fallback, wrapper = (o) => o) {
     if(typeof name !== 'string') throw new TypeError(`Cannot convert '${typeof b}' to 'string'`);
     if(typeof test !== 'function') throw new TypeError(`Cannot convert '${typeof c}' to 'string'`);
-    this.#a = fallback;
-    this.#b = name;
-    this.#c = test;
-    this.#d = wrapper;
+    this.#f = fallback;
+    this.#n = name;
+    this.#t = test;
+    this.#w = wrapper;
   }
-  get fallback() {return this.#a}
-  get name() {return this.#b}
-  allows(o) {return this.#c(o)}
-  wrap(o) {return this.#d(o)}
+  get fallback() {return this.#f}
+  get name() {return this.#n}
+  allows(o) {return this.#t(o)}
+  wrap(o) {return this.#w(o)}
 }
 class StronglyJS {
   static #TYPES = {
