2c1b9e352ab33b0944ace8a82b05195f9b3205fb
Tim Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2020-11-30 18:51:36 -0700

    Improved tuple type checking

--- strongly.js
+++ strongly.js
@@ -58,7 +58,19 @@ class StronglyJS {
     ANY: new StronglyJSType({}, 'any', (o) => true),
     ARRAYOF: (t) => new StronglyJSType([], t.name + ' array', (o) => o instanceof Array && o.every((i) => t.allows(i))),
     NULLABLE: (t) => new StronglyJSType(null, 'nullable ' + t.name, (o) => o === null || t.allows(o)),
-    TUPLEOF: (...t) => new StronglyJSType(t.map(o => o.fallback), t.map(o => o.name).join(', ') + ' tuple', (o) => o instanceof Array && t.every((i, n) => i.allows(o[n]))),
+    TUPLEOF: (...t) => new StronglyJSType(t.map(o => o.fallback), t.map(o => o.name).join(', ') + ' tuple', (o) => o instanceof Array && t.every((i, n) => i.allows(o[n])),
+    function(l) {return new Proxy(l, {
+      set: (o, p, v) => {
+          let i = parseInt(p);
+          if(Number.isInteger(i) && i < this.data.length && i > -1)
+            if(this.data[i].allows(v)) o[p] = v;
+            else throw new TypeError(`Value '${v}' is not assignable to type '${this.data[i].name}'`);
+          else throw new TypeError(`Cannot modify property '${p}' of '${this.name}'`);
+      },
+      deleteProperty: (o, p) => {
+          throw new TypeError(`Cannot delete property '${p}' of '${this.name}'`);
+      }
+    })}, t),
     UNDEFINED: new StronglyJSType(undefined),
     NULL: new StronglyJSType(null, 'null', (o) => o === null)
   }
